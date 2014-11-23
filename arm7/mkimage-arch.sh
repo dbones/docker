bc3c5c29908a10f265a9a007b28f97ca771810cd
David Rundle <dave@dbones.co.uk>
2014-11-23 23:13:00 +0000

    attempt 3

--- arm7/mkimage-arch.sh
+++ arm7/mkimage-arch.sh
@@ -22,27 +22,27 @@ chmod 755 $ROOTFS
 PKGIGNORE=linux,jfsutils,lvm2,cryptsetup,groff,man-db,man-pages,mdadm,pciutils,pcmciautils,reiserfsprogs,s-nail,xfsprogs
 
 expect <<EOF
-    set send_slow {1 .1}
-    proc send {ignore arg} {
-        sleep .1
-        exp_send -s -- \$arg
-    }
-    set timeout 60
-
-    spawn pacstrap -C ./mkimage-arch-pacman.conf -c -d -G -i $ROOTFS base haveged --ignore $PKGIGNORE
-    expect {
-        -exact "anyway? \[Y/n\] " { send -- "n\r"; exp_continue }
-        -exact "(default=all): " { send -- "\r"; exp_continue }
-        -exact "installation? \[Y/n\]" { send -- "y\r"; exp_continue }
-    }
+	set send_slow {1 .1}
+	proc send {ignore arg} {
+		sleep .1
+		exp_send -s -- \$arg
+	}
+	set timeout 60
+	spawn pacstrap -C ./mkimage-arch-pacman.conf -c -d -G -i $ROOTFS base haveged --ignore $PKGIGNORE
+	expect {
+		-exact "anyway? \[Y/n\] " { send -- "n\r"; exp_continue }
+		-exact "(default=all): " { send -- "\r"; exp_continue }
+		-exact "installation? \[Y/n\]" { send -- "y\r"; exp_continue }
+	}
 EOF
 
-arch-chroot $ROOTFS /bin/sh -c "haveged -w 1024; pacman-key --init; pkill haveged; pacman -Rs --noconfirm haveged"
+arch-chroot $ROOTFS /bin/sh -c "haveged -w 1024; pacman-key --init; pkill haveged; pacman -Rs --noconfirm haveged; pacman-key --populate archlinux"
 arch-chroot $ROOTFS /bin/sh -c "ln -s /usr/share/zoneinfo/UTC /etc/localtime"
 echo 'en_US.UTF-8 UTF-8' > $ROOTFS/etc/locale.gen
 arch-chroot $ROOTFS locale-gen
-arch-chroot $ROOTFS /bin/sh -c 'echo "Server = http://mirror.archlinuxarm.org/\$arch/\$repo" > /etc/pacman.d/mirrorlist; pacman --noconfirm -Sy; pacman --noconfirm -S archlinuxarm-keyring'
+arch-chroot $ROOTFS /bin/sh -c 'echo "Server = http://mirror.archlinuxarm.org/\$arch/\$repo" > /etc/pacman.d/mirrorlist'
 
+# ; pacman --noconfirm -Sy; pacman --noconfirm -S archlinuxarm-keyring
 # udev doesn't work in containers, rebuild /dev
 DEV=$ROOTFS/dev
 rm -rf $DEV
@@ -61,7 +61,7 @@ mknod -m 600 $DEV/initctl p
 mknod -m 666 $DEV/ptmx c 5 2
 ln -sf /proc/self/fd $DEV/fd
 
-tar --numeric-owner -C $ROOTFS -c . | docker import - armhf-archlinux
+tar --numeric-owner --xattrs --acls -C $ROOTFS -c . | docker import - archlinux
 docker run -i -t armhf-archlinux echo Success.
 sleep 2
 rm -rf $ROOTFS
